712b993bcc6e38c667e127814272d421f77da7b7
Matty Ricks <ricklefsmatthew1003@gmail.com>
2022-01-18 15:17:54 -0500

    implements native url module so that url parameters are accessible and dynamically adjustable per request made

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const url = require("url");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
@@ -14,9 +15,15 @@ app.use(cors());
 // routes
 app.get("/api/search/:query", async (req, res) => {
   try {
+    // add api key & query strings
+    const params = new URLSearchParams({
+      access_token: process.env.API_KEY,
+      ...url.parse(req.url, true).query,
+    });
+    console.log(params);
     const query = req.params.query;
     const results = await axios(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.API_KEY}`
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`
     );
     const data = results.data;
     res.status(200).json(data);
