e0d9f61ad95de64fca3e5c886251b260f2ed1043
Matty Ricks <ricklefsmatthew1003@gmail.com>
2022-01-18 15:12:45 -0500

    tests mapbox api via get request on postman, utilizes accesstoken for mapbox and dynamic query property that reflects user search input, success

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+// initialize express
+const app = express();
+
+// enable CORS
+app.use(cors());
+
+// routes
+app.get("/api/search/:query", async (req, res) => {
+  try {
+    const query = req.params.query;
+    const results = await axios(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.API_KEY}`
+    );
+    const data = results.data;
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
