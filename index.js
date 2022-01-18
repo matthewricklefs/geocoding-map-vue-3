6a75eabde4a1a8297db32104b9325f3e0d919ec1
Matty Ricks <ricklefsmatthew1003@gmail.com>
2022-01-18 15:25:51 -0500

    migrates searchQuery route to routes/ to clean up entrypoint

--- index.js
+++ index.js
@@ -13,23 +13,6 @@ const app = express();
 app.use(cors());
 
 // routes
-app.get("/api/search/:query", async (req, res) => {
-  try {
-    // add api key & query strings
-    const params = new URLSearchParams({
-      access_token: process.env.API_KEY,
-      ...url.parse(req.url, true).query,
-    });
-    console.log(params);
-    const query = req.params.query;
-    const results = await axios(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`
-    );
-    const data = results.data;
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+app.use("/api/search", require("./routes/searchResults.js"));
 
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
