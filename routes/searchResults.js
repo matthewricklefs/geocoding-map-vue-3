b66c63b06358476d8798a5ad68a6c70628dd8be6
Matty Ricks <ricklefsmatthew1003@gmail.com>
2022-01-19 18:01:37 -0500

    restructuring the data so that in addition to a base search queue, we can also add specific city and state targets

--- routes/searchResults.js
+++ routes/searchResults.js
@@ -10,11 +10,30 @@ router.get("/:query", async (req, res) => {
       access_token: process.env.API_KEY,
       ...url.parse(req.url, true).query,
     });
+    // fetching from geocoding search API
     const query = req.params.query;
     const results = await axios(
       `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`
     );
+
+    // formatting data to target city and state
     const data = results.data;
+    data.features.forEach((item) => {
+      // initially set to null
+      item.city = null;
+      item.state = null;
+
+      // cycle through content results
+      item.context.forEach((type) => {
+        if (type.id.includes("price")) {
+          item.city = type.text;
+        }
+        if (type.id.includes("region")) {
+          item.state = type.text;
+        }
+      });
+    });
+
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
